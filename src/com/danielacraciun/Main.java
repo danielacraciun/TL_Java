1d56c68145f988581a8616df7f2a408f2f78e27d
Daniela Craciun <craciundaniela19@gmail.com>
2015-11-02 19:49:36 +0200

    Added main packages

---
+++ src/com/danielacraciun/Main.java
@@ -0,0 +1,34 @@
+package com.danielacraciun;
+
+class Main {
+
+    public static void main(String[] args) {
+//        IRepository repo = new Repository();
+//        Controller ctrl = new Controller(repo);
+//        Console console = new Console(ctrl);
+//        console.run();
+//        Stack stk = new ArrayStack();
+//        Dictionary d = new ArrayDictionary();
+//        List l = new ArrayList();
+//
+//        IStmt prg1 = new CmpStmt(new AssignStmt("a", new ConstExp(0)),
+//                new AssignStmt ("b", new ArithmExp(new VarExp("a"),
+//                        new ConstExp(2), '*')));
+//        IStmt prg2 = new CmpStmt (new AssignStmt ("c",
+//                new ArithmExp(new VarExp("a"), new ConstExp(2), '+')),
+//                new IfStmt(new VarExp("a"),
+//                        new PrintStmt(new ArithmExp(new VarExp("b"), new VarExp("c"), '+')),
+//                        new PrintStmt(new ArithmExp(new VarExp("b"), new VarExp("c"), '-'))));
+//        IStmt fprg = new CmpStmt (prg1, prg2);
+//
+//        stk.push(fprg);
+//        PrgState ps = new PrgState (stk, d, l);
+//
+//        IRepository repo = new Repository();
+//        repo.add(ps);
+//        Controller ctrl = new Controller(repo);
+//
+//        ctrl.fullStep(true);
+        System.out.println("Hello!");
+    }
+}
